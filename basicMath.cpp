00091e564e589b98a2bea39942709080fd2f828e
hiab gebremichael <hiab.gebremichael.stu.ocsb.ca>
2025-02-19 23:36:40 +0000

    Added basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+std::cout<<"9 + 2 = "<< 9+2 << std::endl;
+std::cout<<"8 - 7 = " << 8-7 << std::endl;
+std::cout<<"8 / 2 = " << 8/2 << std::endl;
+std::cout<< "12 - 2 / 2 = " << 12-2/2 << std::endl;
+std::cout<< "3^2 = " << (3^2) << std::endl;
+}
